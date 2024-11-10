173a4d1b70ef90097b7672dcf58b94e5067cea05
Bavin <bavinachu465@gmail.com>
2024-11-10 12:10:35 +0530

    program to number of duplicate elements in a list

---
+++ bin/sample.dart
@@ -0,0 +1,12 @@
+void main() {
+  List<int> num = [1, 1, 3, 7, 7, 2, 3, 4, 4, 5];
+  int count = 0;
+  for (int i = 0; i <= num.length - 1; i++) {
+    for (int j = i + 1; j <= num.length - 1; j++) {
+      if (num[j] == num[i]) {
+        count++;
+      }
+    }
+  }
+  print('Total number of duplicates in the list is $count');
+}
