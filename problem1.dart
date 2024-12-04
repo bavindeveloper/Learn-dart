e85ab332b682fbaee10c1937a1a3bf856d541392
Bavin <bavinachu465@gmail.com>
2024-12-04 10:46:36 +0530

    accept a char input and display it on the console

---
+++ problem1.dart
@@ -0,0 +1,14 @@
+//⦁	Accept a char input from the user and display it on the console.
+import 'dart:io';
+
+void main() {
+  print('enter a character ');
+  var a = stdin.readLineSync();
+  print('  character is $a');
+}
+
+// output
+// --------------
+// enter a character 
+// 456789
+//   character is 456789
